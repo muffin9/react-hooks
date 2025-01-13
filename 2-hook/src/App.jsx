356f5df3c089c6588c906001d7351a511dbe74b8
muffin9 <jinha6707@gmail.com>
2025-01-13 17:46:13 +0900

    feat: useEffect 함수 만들기

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,5 +1,5 @@
 import MyReact from '../../shared/lib/MyReact.jsx';
-
+import React from 'react';
 // const App = () => <>2-hook</>;
 
 // export default App;
@@ -15,4 +15,51 @@ function NameField() {
   return <input value={name} onChange={handleChange} />;
 }
 
-export default () => <NameField />;
+const Counter = () => {
+  MyReact.resetCursor();
+
+  const [count, setCount] = React.useState(0);
+  const [name, setName] = React.useState('');
+
+  const handleClick = () => setCount(count + 1);
+
+  const handleChangeName = (e) => setName(e.target.value);
+
+  MyReact.useEffect(() => {
+    document.title = `count: ${count}`;
+    console.log('effect1');
+  }, [count]);
+
+  MyReact.useEffect(() => {
+    localStorage.setItem('name', name);
+    console.log('effect2');
+  }, [name]);
+
+  MyReact.useEffect(() => {
+    setName(localStorage.getItem('name') || '');
+  }, []);
+
+  return (
+    <>
+      <button onClick={handleClick}>더하기</button>
+      <input value={name} onChange={handleChangeName} />
+    </>
+  );
+};
+
+export default () => {
+  const [mounted, setMounted] = React.useState(false);
+
+  const handleToggle = () => {
+    const nextMounted = !mounted;
+    if (!nextMounted) MyReact.cleanupEffects();
+    setMounted(nextMounted);
+  };
+
+  return (
+    <>
+      <button onClick={handleToggle}>컴포넌트 토글</button>
+      {mounted && <Counter />}
+    </>
+  );
+};
